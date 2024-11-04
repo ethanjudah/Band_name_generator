8291a92059d14b3280343d0de8bef8b149865839
ethanjudah <138591713+ethanjudah@users.noreply.github.com>
2024-11-04 17:42:25 +0000

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+#create greeting for program
+print("Welcome To The Band Name Generator")
+#Ask the user for the city that they grew up in and store it in a variable.
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
